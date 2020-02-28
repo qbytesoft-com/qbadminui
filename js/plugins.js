a273c5f6e63fe8524bff94a1f0302468414590b7
Maruf Ahmed <maruf.ahmed76@yahoo.com>
2020-02-29 00:24:44 +0600

    Add 3 new pages

--- js/plugins.js
+++ js/plugins.js
@@ -25,46 +25,50 @@
 
 // Place any jQuery/helper plugins in here.
 $(document).ready(function(){
-    // Navbar script
-    $(".navbar-brand").click(function(){
-        $(".side-bar").toggleClass("side-bar-active");
-        $(".page-body").toggleClass("page-active");
-        $("body").toggleClass("body-active");
-    })
-    $(".page-body").click(function(){
-        $(".side-bar").removeClass("side-bar-active");
-        $(".page-body").removeClass("page-active");
-        $("body").removeClass("body-active");
-    })
+  // Block search button action
+  $(".navb-search > form").submit(function(e){
+    e.preventDefault();
+  })
+  // Navbar script
+  $(".navbar-brand").click(function(){
+      $(".side-bar").toggleClass("side-bar-active");
+      $(".page-body").toggleClass("page-active");
+      $("body").toggleClass("body-active");
+  })
+  $(".page-body").click(function(){
+      $(".side-bar").removeClass("side-bar-active");
+      $(".page-body").removeClass("page-active");
+      $("body").removeClass("body-active");
+  })
 
-    // Theme
+  // Theme
+  setSideNavTheme();
+  $(".theme-pck").click(function(){
+    $(".theme-option").toggleClass("active");
+  })
+  $(".side-nav-theme").click(function(){
+    let theme_color = $(this).attr("theme-color");
+    $(".side-nav-theme").removeClass("active");
+    // Store side nav theme to localstorage
+    let themeOptions = {sideNavTheme : theme_color};
+    localStorage.setItem("theme",JSON.stringify(themeOptions));
     setSideNavTheme();
-    $(".theme-pck").click(function(){
-      $(".theme-option").toggleClass("active");
-    })
-    $(".side-nav-theme").click(function(){
-      let theme_color = $(this).attr("theme-color");
-      $(".side-nav-theme").removeClass("active");
-      // Store side nav theme to localstorage
-      let themeOptions = {sideNavTheme : theme_color};
-      localStorage.setItem("theme",JSON.stringify(themeOptions));
-      setSideNavTheme();
-    })
-    // Set side nav function
-    function setSideNavTheme(){
-      let localstr = localStorage.getItem("theme");
-      if(localstr){
-        let nav_theme = JSON.parse(localstr).sideNavTheme;
+  })
+  // Set side nav function
+  function setSideNavTheme(){
+    let localstr = localStorage.getItem("theme");
+    if(localstr){
+      let nav_theme = JSON.parse(localstr).sideNavTheme;
 
-        if($(".side-bar").attr("data-theme") == "purple")
-        {
-          $(".side-bar").attr("data-theme",nav_theme)
-        }else{
-          $(".side-bar").attr("data-theme",nav_theme)
-        }
+      if($(".side-bar").attr("data-theme") == "purple")
+      {
+        $(".side-bar").attr("data-theme",nav_theme)
+      }else{
+        $(".side-bar").attr("data-theme",nav_theme)
       }
-      
     }
+    
+  }
     
     
 })
